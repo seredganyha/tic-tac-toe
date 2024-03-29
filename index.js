586ade9e927b54b6c673d4fe6561c876f2c60724
seredganyha <karix05@yandex.ru>
2024-03-29 09:21:21 +0300

    add: minimax algoritm

--- index.js
+++ index.js
@@ -143,3 +143,158 @@ function calculateCoordinates(row, column, cellWidth, cellHeight) {
   return { x: x, y: y };
 }
 
+function minimax(board, depth, isMax, alpha, beta) {
+  let score = evaluate(board);
+
+  if (score === 10) {
+      return score - depth; // Max player (AI) WIN
+  }
+
+  if (score === -10) {
+      return score + depth; // Min player (HU) WIN
+  }
+
+  if (!isMovesLeft(board)) {
+      return 0; // TIE
+  }
+
+  if (isMax) {
+      let best = -1000;
+
+      for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 3; j++) {
+              if (board[i][j] === null) {
+                  board[i][j] = aiPlayer;
+                  best = Math.max(best, minimax(board, depth + 1, !isMax, alpha, beta));
+                  board[i][j] = null;
+                  alpha = Math.max(alpha, best);
+                  if (beta <= alpha) {
+                      break;
+                  }
+              }
+          }
+      }
+      return best;
+  } else {
+      let best = 1000;
+
+      for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 3; j++) {
+              if (board[i][j] === null) {
+                  board[i][j] = huPlayer;
+                  best = Math.min(best, minimax(board, depth + 1, !isMax, alpha, beta));
+                  board[i][j] = null;
+                  beta = Math.min(beta, best);
+                  if (beta <= alpha) {
+                      break;
+                  }
+              }
+          }
+      }
+
+      return best;
+  }
+}
+
+function findBestMove(board) {
+  let bestVal = -1000;
+  let bestMove = {
+      row: -1,
+      column: -1
+  };
+
+  for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+          if (board[i][j] === null) {
+              board[i][j] = aiPlayer;
+              let moveVal = minimax(board, 0, false, -1000, 1000);
+              board[i][j] = null;
+
+              if (moveVal > bestVal) {
+                  bestMove.row = i;
+                  bestMove.column = j;
+                  bestVal = moveVal;
+              } else if (moveVal === bestVal) {
+                  let defensiveVal = minimax(board, 0, true, -1000, 1000);
+                  if (defensiveVal < moveVal) {
+                      bestMove.row = i;
+                      bestMove.column = j;
+                      bestVal = moveVal;
+                  }
+              }
+          }
+      }
+  }
+
+  return bestMove;
+}
+
+function isMovesLeft(board) {
+  for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+          if (board[i][j] === null) {
+              return true;
+          }
+      }
+  }
+  return false;
+}
+
+function evaluate(board) {
+  // Rows
+  for (let row = 0; row < 3; row++) {
+      if (board[row][0] === board[row][1] && board[row][1] === board[row][2]) {
+          if (board[row][0] === aiPlayer) {
+              return 10; // AI WIN
+          } else if (board[row][0] === huPlayer) {
+              return -10; // HU WIN
+          }
+      }
+  }
+
+  // Columns
+  for (let col = 0; col < 3; col++) {
+      if (board[0][col] === board[1][col] && board[1][col] === board[2][col]) {
+          if (board[0][col] === aiPlayer) {
+              return 10; // AI WIN
+          } else if (board[0][col] === huPlayer) {
+              return -10; // HU WIN
+          }
+      }
+  }
+
+  // Diagonals
+  if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      if (board[0][0] === aiPlayer) {
+          return 10; // AI WIN
+      } else if (board[0][0] === huPlayer) {
+          return -10; // HU WIN
+      }
+  }
+
+  if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      if (board[0][2] === aiPlayer) {
+          return 10; // AI WIN
+      } else if (board[0][2] === huPlayer) {
+          return -10; // HU WIN
+      }
+  }
+
+  // Tie
+  let tie = true;
+  for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+          if (board[i][j] === null) {
+              tie = false;
+              break;
+          }
+      }
+  }
+
+  if (tie) {
+      return 1; // TIE (positive to MIN PLAYER)
+  }
+
+  // Game is not over
+  return null;
+}
